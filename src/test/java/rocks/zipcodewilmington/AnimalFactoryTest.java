0213b76185561b338df769a1e2d14504ca953963
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-10 16:27:38 -0500

    Finished animalfactory

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,50 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void testCreateDog() {
+        //Given
+        String givenName = "Zula";
+        Date givenBirthDate = new Date();
+
+        //When
+        Dog dog = new Dog(givenName, givenBirthDate, 0);
+        AnimalFactory aF = new AnimalFactory();
+        Dog dog2 = aF.createDog(givenName, givenBirthDate);
+
+        //Then
+        Assert.assertNotEquals(dog2, dog);
+
+    }
+
+    @Test
+    public void testCreateCat(){
+        //Given
+        String givenName = "Zula";
+        Date givenBirthDate = new Date();
+
+        //When
+        Cat cat = new Cat(givenName, givenBirthDate, 0);
+        AnimalFactory aF = new AnimalFactory();
+        Cat cat2 = aF.createCat(givenName, givenBirthDate);
+
+        //Then
+        Assert.assertNotEquals(cat2, cat);
+    }
+
+
 }
